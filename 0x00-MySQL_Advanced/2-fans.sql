981c8f1a27f6f2fb0c03a6a65d1ee436a1275476
Mike-chege <mikechege93@gmail.com>
2023-12-13 17:02:32 +0000

    Ranking bands based on their funs number / Fixed typos

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,6 @@
 -- This script ranks country origins of bands
--- Ordered by the number of (non-unique) fans
 
-SELECT origin, SUM(fans) AS num_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY num_fans DESC;
+ORDER BY nb_fans DESC;
